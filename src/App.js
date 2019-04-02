61c8ca2b634d6426aab1d0e6d112116f851644d0
Michal Majer <majer.michal@outlook.com>
2019-04-02 15:47:57 +1100

    Create website template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './styles/App.css';
+import Navigation from './pages/Navigation';
+import Home from './pages/Home';
+import About from './pages/About';
+import Tools from './pages/Tools';
+import Experience from './pages/Experience';
+import Contact from './pages/Contact';
+import Footer from './pages/Footer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="app">
+          <nav>
+            {<Navigation />}
+          </nav>
+          <section className="home">
+            {<Home />}
+          </section>
+          <section className="about">
+            {<About />}
+          </section>
+          <section className="tools">
+            {<Tools />}
+          </section>
+          <section className="experience">
+            {<Experience />}
+          </section>
+          <section className="contact">
+            {<Contact />}
+          </section>
+          <footer>
+            {<Footer />}
+          </footer>
+        </div>
+      </Router>
     );
   }
 }

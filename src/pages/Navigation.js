b1ee2b8b986ab36ae1ce11c4013be9a4a662db20
Michal Majer <majer.michal@outlook.com>
2019-04-02 16:36:19 +1100

    Add burger menu icon

--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import '../styles/Navigation.css'
 
-const Navigation = () => {
-    return (
-        <div>Navigation</div>
-    );
+class Navigation extends React.Component {
+
+    state = {
+        value: "burger",
+        click: 1,
+    };
+
+    handleClick = () => {
+
+        if (this.state.click === 1) {
+            this.setState({
+                value: "burger active",
+                click: 2
+            })
+        }
+        if (this.state.click === 2) {
+            this.setState({
+                value: "burger close",
+                click: 1
+            })
+        }
+    }
+    render() {
+        return (
+            <div onClick={this.handleClick} className={this.state.value} >
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        );
+    }
 }
-
 export default Navigation;

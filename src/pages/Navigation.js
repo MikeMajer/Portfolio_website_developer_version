b2e4092918096038b6d4fc7204c5da6296ad00f3
Michal Majer <majer.michal@outlook.com>
2019-04-02 19:45:33 +1100

    Add menu background

--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -5,6 +5,7 @@ class Navigation extends React.Component {
 
     state = {
         value: "burger",
+        aside: "",
         click: 1,
     };
 
@@ -13,23 +14,32 @@ class Navigation extends React.Component {
         if (this.state.click === 1) {
             this.setState({
                 value: "burger active",
+                aside: "on",
                 click: 2
             })
         }
         if (this.state.click === 2) {
             this.setState({
                 value: "burger close",
+                aside: "",
                 click: 1
             })
         }
     }
     render() {
         return (
-            <div onClick={this.handleClick} className={this.state.value} >
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
+            <>
+                <div onClick={this.handleClick} className={this.state.value} >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <aside id="one" className={this.state.aside}></aside>
+                <aside id="two" className={this.state.aside}></aside>
+                <aside id="tree" className={this.state.aside}></aside>
+                <aside id="four" className={this.state.aside}></aside>
+
+            </>
         );
     }
 }

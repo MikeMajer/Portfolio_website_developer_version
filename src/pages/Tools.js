f847c5934f37f119ca42f370534cc649c23d35ee
Michal Majer <majer.michal@outlook.com>
2019-04-04 18:43:12 +1100

    Tools page styling

--- src/pages/Tools.js
+++ src/pages/Tools.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import "../styles/Tools.css"
 
 const Tools = () => {
-    return (
-        <div>Tools</div>
-    );
+  return (
+    <>
+      <div className="title"><h1>Tools and technologies I am using</h1></div>
+
+      <div className="icons">
+        <span><i class="fab fa-html5"></i>HTML 5</span>
+        <span><i class="fab fa-css3-alt"></i> CSS 3</span>
+        <span><i class="fab fa-js"></i>JavaScript</span>
+        <span><i class="fab fa-react"></i>React.js</span>
+        <span><i class="fab fa-node"></i>Node.js</span>
+        <span><i class="fab fa-github"></i>Git & GitHub</span>
+      </div>
+    </>
+  );
 }
 
 export default Tools;

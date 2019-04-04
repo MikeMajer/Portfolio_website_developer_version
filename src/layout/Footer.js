1b9c0c22b0161c68967997a22c1f541f373016c7
Michal Majer <majer.michal@outlook.com>
2019-04-04 14:01:50 +1100

    Footer styling

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import "../styles/Footer.css";
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className="footerWrapper">
+            <div className="social">
+                <a href="https://github.com/"><i class="fab fa-github"></i></a>
+                <a href="https://codepen.io/"><i class="fab fa-codepen"></i></a>
+                <a href="https://au.linkedin.com/"><i class="fab fa-linkedin"></i></a>
+            </div>
+            <div className="copyRight">&copy; Mike Majer </div>
+        </div>
     );
 }
 

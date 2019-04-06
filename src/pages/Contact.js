c11c9f591882d58035649cfa65ca4536793166f8
Michal Majer <majer.michal@outlook.com>
2019-04-06 19:26:09 +1100

    Create a contact form template

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import "../styles/Contact.css";
+import { Prompt } from 'react-router-dom';
 
-const Contact = () => {
-    return (
-        <div>Contact</div>
-    );
+class Contact extends React.Component {
+    state = {
+        value: "",
+        isEmpty: false
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.setState({
+            value: "",
+            isEmpty: true
+        })
+    }
+
+    handleChange = (e) => {
+        if (e.target.value.lenght > 0) {
+            this.setState({
+                value: e.target.value,
+                isEmpty: true
+            })
+        } else {
+            this.setState({
+                value: e.target.value,
+                isEmpty: false
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div className="contact">
+                <form onSubmit={this.handleSubmit}>
+                    <h1>Contact me here...</h1>
+                    <textarea
+                        value={this.state.value}
+                        on onChange={this.handleChange}
+                        placeholder="Type message..."
+                    ></textarea>
+                    <button>Send</button>
+                </form>
+                <Prompt
+                    when={this.state.value}
+                    message="Your form is not submitted. Are you sure that you want to leave this page?"
+                />
+            </div>
+        );
+    }
 }
-
 export default Contact;

13cc1294f99c8a887bf8bf7e5c3bb4b4308ca46b
Michal Majer <majer.michal@outlook.com>
2019-04-08 15:01:09 +1000

    Updates

---
+++ precache-manifest.b960f6d978797768855789cc39d7a40f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "013035c24be5956ff0803b7670df5df5",
+    "url": "/MikeMajer/Portfolio_website_developer_version.git/static/media/lion.013035c2.jpg"
+  },
+  {
+    "revision": "3dd28b53f02db095b2ce50bc733f1ae4",
+    "url": "/MikeMajer/Portfolio_website_developer_version.git/static/media/img.3dd28b53.png"
+  },
+  {
+    "revision": "93993e106ade8dca6b86a91a44d9adc5",
+    "url": "/MikeMajer/Portfolio_website_developer_version.git/static/media/brush-stroke-banner.93993e10.svg"
+  },
+  {
+    "revision": "da89754846e5f2380145",
+    "url": "/MikeMajer/Portfolio_website_developer_version.git/static/js/runtime~main.3c3daa39.js"
+  },
+  {
+    "revision": "3affca877aaab8df187a",
+    "url": "/MikeMajer/Portfolio_website_developer_version.git/static/js/main.c372a65f.chunk.js"
+  },
+  {
+    "revision": "12202dd3c0322ace0d06",
+    "url": "/MikeMajer/Portfolio_website_developer_version.git/static/js/2.d1b0cb80.chunk.js"
+  },
+  {
+    "revision": "3affca877aaab8df187a",
+    "url": "/MikeMajer/Portfolio_website_developer_version.git/static/css/main.dbb09705.chunk.css"
+  },
+  {
+    "revision": "643d6b84e4ef242e9472e2bfaa562efd",
+    "url": "/MikeMajer/Portfolio_website_developer_version.git/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/MikeMajer/Portfolio_website_developer_version.git/precache-manifest.b960f6d978797768855789cc39d7a40f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/MikeMajer/Portfolio_website_developer_version.git/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});

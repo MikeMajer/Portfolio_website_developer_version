d7b35fcbd997836cd1c4c4314c282d2e272d877b
Michal Majer <majer.michal@outlook.com>
2019-04-08 15:34:30 +1000

    Create an error page template

--- src/layout/Pages.js
+++ src/layout/Pages.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';
 import Tools from '../pages/Tools';
 import Experience from '../pages/Experience';
 import Contact from '../pages/Contact';
+import ErrorPage from '../pages/ErrorPage';
+
 
 const Pages = () => {
     return (
         <>
-            <Route path="/" exact component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/tools" component={Tools} />
-            <Route path="/experience" component={Experience} />
-            <Route path="/contact" component={Contact} />
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/tools" component={Tools} />
+                <Route path="/experience" component={Experience} />
+                <Route path="/contact" component={Contact} />
+                <Route component={ErrorPage} />
+            </Switch>
         </>
 
     );
---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../styles/Error.css";
+
+
+const ErrorPage = () => {
+    return (
+        <div className="error"><h1> The page doesn't exist ! </h1></div>
+    );
+}
+
+export default ErrorPage;

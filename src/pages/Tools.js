1d540f03b0ff9e449ba7d9f7946e500773c13dcd
Michal Majer <majer.michal@outlook.com>
2019-04-20 17:58:21 +1000

    Add media queris

--- src/pages/Tools.js
+++ src/pages/Tools.js
@@ -2,20 +2,37 @@ import React from 'react';
 import "../styles/Tools.css"
 
 const Tools = () => {
+
+
+  const scrollListen = () => {
+    const icons = document.querySelector(".icons");
+    const arrow = document.querySelector(".scrollLabel");
+    const scrollLenght = icons.scrollHeight - icons.clientHeight;
+
+    if (icons.scrollTop === scrollLenght) {
+      arrow.classList.add("scrollEnd");
+    } else if (icons.scrollTop === 0) {
+      arrow.classList.remove("scrollEnd");
+    }
+
+  }
+
   return (
     <>
       <div className="title"><h1>Tools and technologies I am using</h1></div>
 
-      <div className="icons">
+      <div onScroll={scrollListen} className="icons">
         <span><i className="fab fa-html5"></i>HTML 5</span>
         <span><i className="fab fa-css3-alt"></i> CSS 3</span>
         <span><i className="fab fa-js"></i>JavaScript</span>
         <span><i className="fab fa-react"></i>React.js</span>
         <span><i className="fab fa-node"></i>Node.js</span>
-        <span><i className="fab fa-github"></i>Git & GitHub</span>
+        <span><i className="fab fa-github"></i>Git &amp; GitHub</span>
       </div>
+      <label className="scrollLabel"><i className="fas fa-angle-down"></i></label>
     </>
   );
 }
+
 
 export default Tools;

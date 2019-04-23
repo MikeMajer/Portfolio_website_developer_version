819b7d552d15f5c418fdf70338c5c0b9c0243f84
Michal Majer <majer.michal@outlook.com>
2019-04-23 22:11:04 +1000

    Media queries part III

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,53 +3,56 @@ import "../styles/Contact.css";
 import { Prompt } from 'react-router-dom';
 
 class Contact extends React.Component {
-    state = {
-        value: "",
-        isEmpty: false
-    }
+  state = {
+    value: "",
+    name: "",
+    email: "",
+    subject: ""
+  }
 
-    handleSubmit = (e) => {
-        e.preventDefault()
-        alert("Sorry for any inconvenience, form is under constraction at the moment. Please conatact in majer.michal@outlook.com or through my Liknedin page.")
-        this.setState({
-            value: "",
-            isEmpty: true
-        })
-    }
+  handleSubmit = (e) => {
+    e.preventDefault()
+    alert("Sorry for any inconvenience, the form is under construction at the moment. Please contact me on majer.michal@outlook.com or through my LinkedIn profile page.")
+    this.setState({
+      value: "",
+      name: "",
+      email: "",
+      subject: ""
+    })
+  }
 
-    handleChange = (e) => {
-        if (e.target.value.lenght > 0) {
-            this.setState({
-                value: e.target.value,
-                isEmpty: true
-            })
-        } else {
-            this.setState({
-                value: e.target.value,
-                isEmpty: false
-            })
-        }
-    }
+  handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
 
-    render() {
-        return (
-            <div className="contact">
-                <form onSubmit={this.handleSubmit}>
-                    <h1>Contact me here ...</h1>
-                    <textarea
-                        value={this.state.value}
-                        on onChange={this.handleChange}
-                        placeholder="Type message..."
-                    >
-                    </textarea>
-                    <button>Send</button>
-                </form>
-                <Prompt
-                    when={this.state.value}
-                    message="Your form is not submitted. Are you sure that you want to leave this page?"
-                />
-            </div>
-        );
-    }
+    this.setState({
+      [name]: value
+    })
+  }
+
+  render() {
+    return (
+      <div className="contact">
+        <h1>Contact me here ...</h1>
+        <form className="formStyle" onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange} type="text" placeholder="Name ..." name="name" value={this.state.name} autoComplete="off" />
+          <input onChange={this.handleChange} type="email" placeholder="Email ..." name="email" value={this.state.email} autoComplete="off" />
+          <input onChange={this.handleChange} type="text" placeholder="Subject ..." name="subject" value={this.state.subject} autoComplete="off" />
+          <textarea
+            name="value"
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="Type message ..."
+          >
+          </textarea>
+          <button>Submit</button>
+        </form>
+        <Prompt
+          when={this.state.value}
+          message="Your form is not submitted. Are you sure that you want to leave this page?"
+        />
+      </div>
+    );
+  }
 }
 export default Contact;

790a645259b8e01a0521c7be719e089aa6024e36
Michal Majer <majer.michal@outlook.com>
2019-04-24 10:58:58 +1000

    Media queries correction

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,6 +3,7 @@ import "../styles/Contact.css";
 import { Prompt } from 'react-router-dom';
 
 class Contact extends React.Component {
+
   state = {
     value: "",
     name: "",
@@ -11,6 +12,7 @@ class Contact extends React.Component {
   }
 
   handleSubmit = (e) => {
+
     e.preventDefault()
     alert("Sorry for any inconvenience, the form is under construction at the moment. Please contact me on majer.michal@outlook.com or through my LinkedIn profile page.")
     this.setState({
@@ -19,15 +21,17 @@ class Contact extends React.Component {
       email: "",
       subject: ""
     })
+
   }
 
   handleChange = (e) => {
+
     const name = e.target.name;
     const value = e.target.value;
-
     this.setState({
       [name]: value
     })
+
   }
 
   render() {
@@ -35,9 +39,30 @@ class Contact extends React.Component {
       <div className="contact">
         <h1>Contact me here ...</h1>
         <form className="formStyle" onSubmit={this.handleSubmit}>
-          <input onChange={this.handleChange} type="text" placeholder="Name ..." name="name" value={this.state.name} autoComplete="off" />
-          <input onChange={this.handleChange} type="email" placeholder="Email ..." name="email" value={this.state.email} autoComplete="off" />
-          <input onChange={this.handleChange} type="text" placeholder="Subject ..." name="subject" value={this.state.subject} autoComplete="off" />
+          <input
+            onChange={this.handleChange}
+            type="text"
+            placeholder="Name ..."
+            name="name"
+            value={this.state.name}
+            autoComplete="off"
+          />
+          <input
+            onChange={this.handleChange}
+            type="email"
+            placeholder="Email ..."
+            name="email"
+            value={this.state.email}
+            autoComplete="off"
+          />
+          <input
+            onChange={this.handleChange}
+            type="text"
+            placeholder="Subject ..."
+            name="subject"
+            value={this.state.subject}
+            autoComplete="off"
+          />
           <textarea
             name="value"
             value={this.state.value}
